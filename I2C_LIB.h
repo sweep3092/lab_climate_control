a9e35d7b38bf60fd1e328251a5c38a30fbed338c
You Kobayashi <sweep.3092@gmail.com>
2015-07-06 18:12:00 +0900

    Initial Commit

---
+++ I2C_LIB.h
@@ -0,0 +1,56 @@
+#define I2Cadr 0x3e  // 固定 I2C用アドレス
+byte contrast = 30;   // コントラスト(0～63)
+
+void lcd_cmd(byte x);
+
+
+//***** I2C_LCD 初期化関数 （必須）
+void lcd_init(void) {   // I2C_LCDの初期化
+   Wire.begin();
+  lcd_cmd(0x38); lcd_cmd(0x39); lcd_cmd(0x4); lcd_cmd(0x14);
+  lcd_cmd(0x70 | (contrast & 0xF)); lcd_cmd(0x5C | ((contrast>>4) &0x3));
+  lcd_cmd(0x6C); delay(200); lcd_cmd(0x38); lcd_cmd(0x0C); lcd_cmd(0x01);
+  delay(2);
+}
+//***** I2C_LCDへの書込み
+void lcd_cmd(byte x) {// I2C_LCDへの書き込み
+  Wire.beginTransmission(I2Cadr);
+  Wire.write(0x00); // CO = 0,RS = 0
+  Wire.write(x);
+  Wire.endTransmission();
+}
+//***** 画面クリア 関数
+void lcd_clear(void) {  lcd_cmd(0x01); } 
+//***** 画面表示Off 関数
+void lcd_DisplayOff() {  lcd_cmd(0x08); }
+//***** 画面表示On 関数
+void lcd_DisplayOn() { lcd_cmd(0x0C);  }
+//===== サブ関数1 
+void lcd_contdata(byte x) {
+Wire.write(0xC0); // CO = 1, RS = 1
+  Wire.write(x);
+}
+//===== サブ関数２ 
+void lcd_lastdata(byte x) {
+  Wire.write(0x40); // CO = 0, RS = 1
+  Wire.write(x);
+}
+//***** 文字の表示関数
+void lcd_printStr(const char *s) {
+  Wire.beginTransmission(I2Cadr);
+  while (*s) {
+    if (*(s + 1)) {
+      lcd_contdata(*s);
+    } else {
+      lcd_lastdata(*s);
+    }
+    s++;
+  }
+  Wire.endTransmission();
+}
+ 
+//***** 表示位置の指定関数 ｘ:桁(0～7)、y：行(0,1）
+void lcd_setCursor(byte x, byte y) {
+  lcd_cmd(0x80 | (y * 0x40 + x));
+}
+
---
+++ lab_climate_control.ino
@@ -0,0 +1,101 @@
+#include "DHT.h"
+#include <Wire.h>
+#include "I2C_LIB.h"
+
+#define RELAY  2
+#define DHTPIN 3
+
+#define TEMPUP   12
+#define TEMPDOWN 13
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+int preset_temp = 26;
+
+int relay_delay = 0;
+
+boolean chtsw(byte dx) {
+  boolean tsw = digitalRead(dx);
+  while (tsw == digitalRead(dx));
+  delay(300);
+  
+  return !tsw;
+}
+
+
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(RELAY, OUTPUT);
+  pinMode(TEMPUP, INPUT_PULLUP);
+  pinMode(TEMPDOWN, INPUT_PULLUP);
+  
+  // LCD
+  pinMode(A2, OUTPUT);
+  digitalWrite(A2, HIGH);
+  pinMode(A3, OUTPUT);
+  digitalWrite(A3, OUTPUT);
+  
+  delay(100);
+  lcd_init();
+  lcd_setCursor(0, 0);
+  lcd_printStr("Climate");
+  lcd_setCursor(0, 1);
+  lcd_printStr("Control");
+  
+  
+  delay(1000);
+  
+  dht.begin();
+}
+
+void loop() {
+  char lcd_upper[8];
+  char lcd_lower[8];
+  
+  delay(200);
+
+  if (digitalRead(TEMPUP) == LOW) {
+    preset_temp++;
+  }
+  if (digitalRead(TEMPDOWN) == LOW) {
+    preset_temp--;
+  }
+  
+  float t = dht.readTemperature();
+  if(isnan(t)){
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  lcd_clear();
+  sprintf(lcd_upper, "Set: %dC", preset_temp);
+  lcd_setCursor(0, 0);
+  lcd_printStr(lcd_upper);
+  sprintf(lcd_lower, "Cur: %dC", round(t));
+  lcd_setCursor(0, 1);
+  lcd_printStr(lcd_lower);
+  
+  relay_delay++;
+  if (relay_delay > 65534) {
+   relay_delay = 300;
+  }
+  
+//  Serial.println(relay_delay);
+  
+  if (relay_delay > 300) {
+    if (round(t) > preset_temp) {
+      // スイッチオン！
+      digitalWrite(RELAY, HIGH);
+//      Serial.println("HIGH");
+    }
+    else {
+      // スイッチオフ！
+      digitalWrite(RELAY, LOW);
+//      Serial.println("LOW");
+    }
+    relay_delay = 0;
+  }
+}
